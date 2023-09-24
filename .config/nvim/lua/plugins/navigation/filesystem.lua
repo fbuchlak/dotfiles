5fba229af77f43df9fd969d9c26eed887207250f
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2023-09-24 18:48:12 +0200

    stop treesitter if mini.files preview is too big

--- .config/nvim/lua/plugins/navigation/filesystem.lua
+++ .config/nvim/lua/plugins/navigation/filesystem.lua
@@ -45,27 +45,48 @@ return {
                 go_in_plus = "l",
             },
             windows = {
-                preview = false,
+                preview = true,
                 width_focus = 35,
                 width_no_focus = 10,
                 width_preview = 70,
             },
         },
         config = function(_, opts)
-            util.create_autocmd("User", "MiniFilesWindowOpen", {
-                pattern = "MiniFilesWindowOpen",
-                callback = function(args)
-                    vim.api.nvim_win_set_config(args.data.win_id, { border = defaults.border })
-                end,
-            }).create_autocmd("User", "MiniFilesBindings", {
-                pattern = "MiniFilesBufferCreate",
-                callback = function(args)
-                    local buffer = args.data.buf_id
+            util
+                --
+                .create_autocmd("User", "MiniFilesWindowOpen", {
+                    pattern = "MiniFilesWindowOpen",
+                    callback = function(args)
+                        vim.api.nvim_win_set_config(args.data.win_id, { border = defaults.border })
+                    end,
+                })
+                .create_autocmd("User", "MiniFilesBindings", {
+                    pattern = "MiniFilesBufferCreate",
+                    callback = function(args)
+                        local bufnr = args.data.buf_id
 
-                    util.map({ "n", "i", "v" }, "<C-s>", require("mini.files").synchronize, { buffer = buffer })
-                        .map("n", "<ESC>", require("mini.files").close, { buffer = buffer })
-                end,
-            })
+                        util.map({ "n", "i", "v" }, "<C-s>", require("mini.files").synchronize, { buffer = bufnr })
+                            .map("n", "<ESC>", require("mini.files").close, { buffer = bufnr })
+                    end,
+                })
+                .create_autocmd("User", "MiniFilesStopTreesitterForBigFile", {
+                    pattern = "MiniFilesBufferUpdate",
+                    callback = function(args)
+                        local bufnr = args.data.buf_id
+
+                        if vim.api.nvim_buf_is_loaded(bufnr) then
+                            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+                            for _, line in pairs(lines) do
+                                if #line > 5000 then
+                                    vim.treesitter.stop(bufnr)
+                                    vim.bo[bufnr].syntax = nil
+
+                                    break
+                                end
+                            end
+                        end
+                    end,
+                })
 
             require("mini.files").setup(opts)
         end,
